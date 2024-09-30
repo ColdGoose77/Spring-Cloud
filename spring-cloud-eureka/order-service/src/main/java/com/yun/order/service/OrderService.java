f6416701f0359a9032a803eee06b18877d871869
ColdGoose77 <2284858868@qq.com>
2024-09-30 17:15:02 +0800

    服务注册和服务发现完成

--- spring-cloud-eureka/order-service/src/main/java/com/yun/order/service/OrderService.java
+++ spring-cloud-eureka/order-service/src/main/java/com/yun/order/service/OrderService.java
@@ -1,11 +1,15 @@
 package com.yun.order.service;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import com.yun.order.mapper.OrderMapper;
 import com.yun.order.model.OrderInfo;
 import com.yun.order.model.ProductInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author yun
@@ -14,17 +18,24 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @Service
+@Slf4j
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
 
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public OrderInfo selectOrderById(Integer orderId) {
         OrderInfo orderInfo = orderMapper.selectOrderById(orderId);
-        String url = "http://localhost:58081/product/1001";
+//        String url = "http://localhost:58081/product/1001";
+        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
+        String uri = instances.get(0).getUri().toString();
+        String url = uri + "/product/" + orderInfo.getProductId();
         ProductInfo productInfo = restTemplate.getForObject(url, ProductInfo.class);
         orderInfo.setProductInfo(productInfo);
 
