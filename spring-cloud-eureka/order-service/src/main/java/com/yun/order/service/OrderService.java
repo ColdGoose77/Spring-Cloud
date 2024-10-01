c5761dcc62fc4994747e7aaa01a396e773c3aa3c
ColdGoose77 <2284858868@qq.com>
2024-10-01 21:37:26 +0800

    负载均衡实现

--- spring-cloud-eureka/order-service/src/main/java/com/yun/order/service/OrderService.java
+++ spring-cloud-eureka/order-service/src/main/java/com/yun/order/service/OrderService.java
@@ -1,4 +1,6 @@
 package com.yun.order.service;
+
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import com.yun.order.mapper.OrderMapper;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author yun
@@ -30,16 +33,37 @@ public class OrderService {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    private AtomicInteger count = new AtomicInteger(1);
+
+    private List<ServiceInstance> instances;
+
+//    public OrderInfo selectOrderById(Integer orderId) {
+//        OrderInfo orderInfo = orderMapper.selectOrderById(orderId);
+////        String url = "http://localhost:58081/product/1001";
+//        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
+//        String uri = instances.get(0).getUri().toString();
+//        String url = uri + "/product/" + orderInfo.getProductId();
+//        ProductInfo productInfo = restTemplate.getForObject(url, ProductInfo.class);
+//        orderInfo.setProductInfo(productInfo);
+//
+//        return orderInfo;
+//    }
+
+    @PostConstruct
+    public void init() {
+        instances = discoveryClient.getInstances("product-service");
+    }
+
     public OrderInfo selectOrderById(Integer orderId) {
         OrderInfo orderInfo = orderMapper.selectOrderById(orderId);
 //        String url = "http://localhost:58081/product/1001";
-        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
-        String uri = instances.get(0).getUri().toString();
+        int index = count.getAndIncrement() % instances.size();
+        String uri = instances.get(index).getUri().toString();
         String url = uri + "/product/" + orderInfo.getProductId();
+        log.info("远程调用url:{}", url);
         ProductInfo productInfo = restTemplate.getForObject(url, ProductInfo.class);
         orderInfo.setProductInfo(productInfo);
 
         return orderInfo;
     }
-
 }
